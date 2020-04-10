d00f7e3651dc20cc337421724eb5e53aacc3e1e8
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-10 15:29:04 +0100

    Updates

---
+++ precache-manifest.43ebac8d3220cca02d77dab3f52bf234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdea86db86273e5a794fbce651338b0",
+    "url": "/Stopwatch/index.html"
+  },
+  {
+    "revision": "c492b36f6d2c44631f13",
+    "url": "/Stopwatch/static/css/main.12e21192.chunk.css"
+  },
+  {
+    "revision": "0e5f737ecdd9bbcf0b9b",
+    "url": "/Stopwatch/static/js/2.37eea5a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stopwatch/static/js/2.37eea5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c492b36f6d2c44631f13",
+    "url": "/Stopwatch/static/js/main.9304e1a8.chunk.js"
+  },
+  {
+    "revision": "d7d0b565d8d79d40efb8",
+    "url": "/Stopwatch/static/js/runtime-main.e3d8ff0d.js"
+  }
+]);
